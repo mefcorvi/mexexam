9631cdd46998c7a807bc845e7fc5d384423a4a52
Bulat Aikaev <mefcorvi@gmail.com>
2025-06-29 12:30:19 +0400

    refactor: remove background image styling for coat.svg from GeneralPage.vue to improve performance

--- src/components/GeneralPage.vue
+++ src/components/GeneralPage.vue
@@ -77,24 +77,6 @@ const back = () => {
   min-height: 100%;
 }
 
-.page::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: -1;
-
-  width: 100%;
-  height: 100%;
-
-  background: url('@/assets/coat.svg') no-repeat center center;
-
-  opacity: 0.05;
-}
-
-
 .topBar {
   position: fixed;
   top: 0;
